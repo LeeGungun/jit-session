6be293eec0f979819c8df416a09eda758498906b
LeeGungun <2625428036@qq.com>
2023-05-24 15:40:38 +0800

    feat: init this project

---
+++ src/main/java/com/lqx/JitSessionApplication.java
@@ -0,0 +1,13 @@
+package com.lqx;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class JitSessionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JitSessionApplication.class, args);
+    }
+}
